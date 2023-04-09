9769e16eb6054d828ca6b5435b01e2d56ac9539a
Nicholas A <bobby.nicholas23@gmail.com>
2023-04-09 13:59:17 +0900

    add logo

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,14 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrownLogo } from '../../../assets/crown.svg'
+
+import './navigation.styles.scss';
 
 const Navigation = () => {
   return (
-    <div>
-      <div>
-        <h1>I AM THE NAVIGATION</h1>
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to='/'>
+          <CrownLogo className="logo" />
+        </Link>
+
+        <div className="nav-links-container">
+          <Link className="nav-link" to='/shop'>
+            SHOP
+          </Link>
+        </div>
       </div>
       <Outlet />
-    </div>
-  )
+    </Fragment>
+  );
 }
 
 export default Navigation;
